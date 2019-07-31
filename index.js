0108494e1cd421b8241edb6b3109a88d9b67af0f
Boris Chan <boris1022chan@gmail.com>
2019-07-30 22:45:59 -0400

    change styling of dark mode toggle

--- index.js
+++ index.js
@@ -2,43 +2,25 @@
 const STORAGE_KEY = 'user-color-scheme';
 const COLOR_MODE_KEY = '--color-mode';
 
-const modeToggleButton = document.getElementById("dark-mode-toggle");
+const lightToggleButton = document.getElementById("light-toggle");
+const darkToggleButton = document.getElementById("dark-toggle");
 
-const getCSSCustomProp = propKey => {
-  let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
-  if (response.length) {
-    response = response.replace(/\"/g, '').trim();
-  }
-  return response;
-};
-
-const applySetting = passedSetting => {
+const applySetting = (passedSetting, shouldStoreSetting) => {
   let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
-  if (currentSetting) {
+  if (currentSetting)
     document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
-  }
+  if (currentSetting && shouldStoreSetting) 
+    localStorage.setItem(STORAGE_KEY, currentSetting);
 };
 
-const toggleSetting = () => {
-  let currentSetting = localStorage.getItem(STORAGE_KEY);
-  switch (currentSetting) {
-    case null:
-      currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
-      break;
-    case 'light':
-      currentSetting = 'dark';
-      break;
-    case 'dark':
-      currentSetting = 'light';
-      break;
-  }
-  localStorage.setItem(STORAGE_KEY, currentSetting);
-  return currentSetting;
-};
-
-modeToggleButton.addEventListener('click', evt => {
+lightToggleButton.addEventListener('click', evt => {
   evt.preventDefault();
-  applySetting(toggleSetting());
+  applySetting('light', true);
+});
+
+darkToggleButton.addEventListener('click', evt => {
+  evt.preventDefault();
+  applySetting('dark', true);
 });
 
 applySetting();
